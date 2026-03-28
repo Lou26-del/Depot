8bed2e3f2d6151123c7da21124fb6f3024391422
Lou26-del <bechirlouisa26@gmail.com>
2026-03-28 19:52:01 +0100

    send

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,10 +1,6 @@
 import pytest
 from flask import Flask
-import sys, os
 from src.public.app import init_routes   # un seul import, cohérent
-
-# Ajouter src/ au chemin
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 @pytest.fixture
 def client():
